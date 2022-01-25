225ab00938c3c67bde89929784268ae941cb44f7
Sunil Kumar <sunil.glbitm@gmail.com>
2022-01-25 23:14:20 +0530

    facebook login integrated

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,20 @@
+class AuthController < ApplicationController
+  def callback
+    omniauth_response = request.env['omniauth.auth']
+    if (omniauth_response)
+      auth = Auth.omniauth(request.env['omniauth.auth'], current_user.try(:id))
+      email = omniauth_response['info'].present? ? omniauth_response['info']['email'] : nil
+      password = SecureRandom.hex(1) + '' + SecureRandom.hex(2).upcase + '' + SecureRandom.hex(3).upcase + '' + SecureRandom.hex(2)
+      if (!current_user)
+        debugger
+        user = User.new(email: email, password: password)
+        user.save
+      end
+      auth.save
+      sign_in user unless current_user
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end  
+  end
+end
